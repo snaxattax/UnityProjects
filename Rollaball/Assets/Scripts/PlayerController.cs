4ea183d6966a9c393fa8e66faeddae4cb0f3183f
Josi Suslov <guitarcrzy44@josisuslovsMBP.fios-router.home>
2020-12-23 19:56:14 -0500

    updates with collectable coins, animations, prefab/kinematic settings

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -28,12 +28,20 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+
+        }
+    }
 
     // Update is called once per frame
     //void Update()
---
+++ Rollaball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+
+    // remember that we set the initial rotations to 45,45,45
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
